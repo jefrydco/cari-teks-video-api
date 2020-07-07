5a4331b8d34c7eb16ef19a52719b7c67b17b0eea
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 03:29:14 +0700

    fix: correct url for get json

--- api/search.ts
+++ api/search.ts
@@ -6,9 +6,9 @@ export default async function handler(req: NowRequest, res: NowResponse) {
   try {
     const url = req.query.url as string || 'https://www.youtube.com/embed/3Fx5Q8xGU8k?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1'
     const q = req.query.q as string || ''
-    // const formattedVtt = await getJson(`${req.headers.host}`)
-    // const searchResult = fuzzySearch(formattedVtt, q)
-    res.send({ searchResult: req.headers.host })
+    const formattedVtt = await getJson(`https://${req.headers.host}/api?${url}`)
+    const searchResult = fuzzySearch(formattedVtt, q)
+    res.send({ searchResult })
   } catch (error) {
     console.error(error)
     res.send(error)
