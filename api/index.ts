5c343bd7982433462bbbaae02c81f4f077d66033
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 01:57:02 +0700

    fix: debug query

--- api/index.ts
+++ api/index.ts
@@ -4,10 +4,10 @@ import getVTT from '../utils/fetch'
 
 export default async function handler(req: NowRequest, res: NowResponse) {
   try {
-    const url = req.query.url as string || 'https://www.youtube.com/embed/3Fx5Q8xGU8k?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1'
+    const url = req.query.yt as string || 'https://www.youtube.com/embed/3Fx5Q8xGU8k?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1'
     const timedTextUrl = await getTimedText(url)
     const vtt = await getVTT(timedTextUrl)
-    res.send({ url })
+    res.send({ query: req.query })
   } catch (error) {
     console.error(error)
     res.send(error)
