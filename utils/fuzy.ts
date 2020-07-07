5c69a569d97d3e9d990486353d94322d6f63d206
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 03:35:26 +0700

    feat: increase fuzzy threshold

--- utils/fuzy.ts
+++ utils/fuzy.ts
@@ -4,7 +4,8 @@ export default function fuzzySearch(list: Array<Record<string, any>>, q: string)
   const fuse = new Fuse(list, {
     keys: ['text'],
     includeMatches: true,
-    minMatchCharLength: 3
+    minMatchCharLength: 3,
+    threshold: 0.3
   })
   return fuse.search(q)
 }
