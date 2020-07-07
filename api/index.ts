481e5f067ea21e4aab448c84e439c1f6789d71b5
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 00:53:09 +0700

    feat: screencapt from youtube

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,8 @@ import getScreenshot from '../utils/browser'
 
 export default async function handler(req: NowRequest, res: NowResponse) {
   try {
-    const file = await getScreenshot('https://jefrydco.id')
+    const url = req.query.url as string || 'https://www.youtube.com/embed/3Fx5Q8xGU8k?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1'
+    const file = await getScreenshot(url)
     res.setHeader("Content-Type", `image/png`);
     res.end(file);
   } catch (error) {
--- utils/browser.ts
+++ utils/browser.ts
@@ -1,16 +1,30 @@
 import chrome from 'chrome-aws-lambda'
 import puppeteer from 'puppeteer-core'
 
-export default async function getScreenshot(url: string, type: 'png' | 'jpeg' = 'png', quality?: number, fullPage?: boolean) {
+export default async function getScreenshot(url: string) {
   const browser = await puppeteer.launch({
       args: chrome.args,
       executablePath: await chrome.executablePath,
       headless: chrome.headless,
-  });
+  })
 
-  const page = await browser.newPage();
-  await page.goto(url);
-  const file = await page.screenshot({ type,  quality, fullPage });
-  await browser.close();
-  return file;
+  const page = await browser.newPage()
+  await page.setRequestInterception(true)
+
+  await page.goto(url, {
+    waitUntil: 'networkidle0'
+  })
+
+  const selector = await page.$('.ytp-subtitles-button ytp-button')
+
+  await page.evaluate(el => {
+    el.click()
+  }, selector)
+
+  const file = await page.screenshot({
+    type: 'png'
+  })
+
+  await browser.close()
+  return file
 }
