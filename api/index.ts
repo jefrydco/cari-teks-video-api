3eaa742b7ce272065bc3e4bb2b17dc6f03c5a369
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 02:05:51 +0700

    feat: add url formatter

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,15 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 import getTimedText from '../utils/browser'
 import getVTT from '../utils/fetch'
+import formatUrl from '../utils/url'
 
 export default async function handler(req: NowRequest, res: NowResponse) {
   try {
-    const url = req.query.yt as string || 'https://www.youtube.com/embed/3Fx5Q8xGU8k?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1'
-    // const timedTextUrl = await getTimedText(url)
-    // const vtt = await getVTT(timedTextUrl)
-    res.send({ query: req.query })
+    const url = req.query.url as string || 'https://www.youtube.com/embed/3Fx5Q8xGU8k?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1'
+    const formattedUrl = formatUrl(url)
+    const timedTextUrl = await getTimedText(formattedUrl)
+    const vtt = await getVTT(timedTextUrl)
+    res.send({ vtt })
   } catch (error) {
     console.error(error)
     res.send(error)
---
+++ utils/url.ts
@@ -0,0 +1,5 @@
+export default function formatUrl(url: string) {
+  const [_, id] = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi
+    .exec(url)
+  return `https://www.youtube.com/embed/${id}?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1`
+}
