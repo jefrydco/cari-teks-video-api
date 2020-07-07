ebdaed2ff5ec507da7ce9929c75e83264efe75ea
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 01:58:24 +0700

    fix: commented out timed text url and vtt

--- api/index.ts
+++ api/index.ts
@@ -5,8 +5,8 @@ import getVTT from '../utils/fetch'
 export default async function handler(req: NowRequest, res: NowResponse) {
   try {
     const url = req.query.yt as string || 'https://www.youtube.com/embed/3Fx5Q8xGU8k?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1'
-    const timedTextUrl = await getTimedText(url)
-    const vtt = await getVTT(timedTextUrl)
+    // const timedTextUrl = await getTimedText(url)
+    // const vtt = await getVTT(timedTextUrl)
     res.send({ query: req.query })
   } catch (error) {
     console.error(error)
