ca1286cff3a2b586da0f822080ffd1754b78008d
Jefry Dewangga <jefrydco@gmail.com>
2020-07-08 02:43:03 +0700

    feat: remove all whitespace more than 1 and new line

--- utils/string.ts
+++ utils/string.ts
@@ -1,5 +1,8 @@
 import stripHtml from 'string-strip-html'
 
 export default function sanitizeString(string: string) {
-  return stripHtml(string).replace(/(<([^>]+)>)/ig, '')
+  return stripHtml(string)
+    .replace(/(<([^>]+)>)/ig, '')
+    .replace(/ {1,}/g, ' ')
+    .replace(/(?:\r\n|\r|\n)/g, '')
 }
