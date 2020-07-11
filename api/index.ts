d7fb978df8824ceafb25ad466ba119652a66e53d
Jefry Dewangga <jefrydco@gmail.com>
2020-07-12 01:57:28 +0700

    debug: page

--- api/index.ts
+++ api/index.ts
@@ -20,16 +20,16 @@ export default async function handler(req: NowRequest, res: NowResponse) {
 
     const url = req.query.url as string
     const formattedUrl = formatUrl(url)
-    logger.info({ formattedUrl }, 'FORMATTED_URL')
+    // logger.info({ formattedUrl }, 'FORMATTED_URL')
 
     const timedTextUrl = await getTimedText(formattedUrl)
-    logger.info({ timedTextUrl }, 'TIMED_TEXT_URL')
+    // logger.info({ timedTextUrl }, 'TIMED_TEXT_URL')
 
     const vtt = await getVTT(timedTextUrl)
-    logger.info({ vtt }, 'VTT')
+    // logger.info({ vtt }, 'VTT')
 
     const strippedVtt = stripHtml(vtt)
-    logger.info({ strippedVtt }, 'STRIPPED_VTT')
+    // logger.info({ strippedVtt }, 'STRIPPED_VTT')
 
     const formattedVtt = vttToJson(strippedVtt)
       .map(item => ({
@@ -37,10 +37,11 @@ export default async function handler(req: NowRequest, res: NowResponse) {
         end: toSecond(item.end as number),
         text: stripWhitespaceNewLine(item.text)
       }))
-    logger.info({ formattedVtt }, 'FORMATTED_VTT')
+    // logger.info({ formattedVtt }, 'FORMATTED_VTT')
 
     if (isExists(req.query.page as string)) {
       const page = parseInt(req.query.page as string)
+      logger.info(String(page), 'PAGE')
       const paginated = paginate(formattedVtt, page)
       return res.send(new ResponseData(paginated, { page }).get())
     }
