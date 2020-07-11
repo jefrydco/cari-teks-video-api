de0c3b3c8f4cf62bd764e9564bce89375349408d
Jefry Dewangga <jefrydco@gmail.com>
2020-07-11 18:41:35 +0700

    debug: formatted vtt

--- api/search.ts
+++ api/search.ts
@@ -15,6 +15,7 @@ export default async function handler(req: NowRequest, res: NowResponse) {
     const url = req.query.url as string
     const q = req.query.q as string
     const formattedVtt = await getJson(`https://${req.headers.host}/api?url=${url}`)
+    res.send(formattedVtt)
     const searchResult = fuzzySearch(formattedVtt, q)
     res.send(new ResponseData(searchResult))
   } catch (error) {
