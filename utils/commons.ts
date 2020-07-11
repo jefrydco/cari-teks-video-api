a94e89579f77ac01fafd5bd35761edcfd07cc8e7
Jefry Dewangga <jefrydco@gmail.com>
2020-07-12 01:45:08 +0700

    debug: page

--- utils/commons.ts
+++ utils/commons.ts
@@ -1,6 +1,7 @@
 import { logger } from "./logger"
 
 export function isExists(data: any) {
+  logger.info(String(data))
   logger.info(String(data !== null && typeof data !== undefined))
   return data !== null && typeof data !== undefined
 }
