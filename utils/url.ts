b5390d78671366cf0659c7e2b216c5f8431dc3c7
Jefry Dewangga <jefrydco@gmail.com>
2020-07-11 20:22:47 +0700

    chore: add comment for regex source

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,5 @@
 export function formatUrl(url: string) {
+  // Taken from: https://stackoverflow.com/a/6904504
   const [_, id] = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi
     .exec(url)
   return `https://www.youtube.com/embed/${id}?hl=en&cc_lang_pref=en&cc_load_policy=1&autoplay=1`
