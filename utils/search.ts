40736744b2709d36c5be4405d63815a7d3d87ef1
Jefry Dewangga <jefrydco@gmail.com>
2020-07-12 18:27:02 +0700

    feat: disable ignore location and set threshold to 0.5

--- utils/search.ts
+++ utils/search.ts
@@ -5,7 +5,7 @@ export function fuzzySearch(list: Array<Record<string, any>>, q: string, marked:
     keys: ['text'],
     includeMatches: marked,
     minMatchCharLength: 3,
-    ignoreLocation: true
+    threshold: 0.5
   })
   if (marked) {
     return markText(fuse.search(q))
