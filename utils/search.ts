69c47a6baede9d8b347061ecf0e2b2b5fce7f431
Jefry Dewangga <jefrydco@gmail.com>
2020-07-17 15:41:04 +0700

    feat: better mark text logic

--- utils/search.ts
+++ utils/search.ts
@@ -13,19 +13,20 @@ export async function flexSearch(list: Vtt[], q: string, marked: boolean = true)
   index.add(list)
   const result = await index.search(q)
   if (marked) {
-    return markTextForFlex(result as Vtt[], q)
+    return markText(result as Vtt[], q)
   }
   return result as Vtt[]
 }
 
-function markTextForFlex(list: Vtt[], q: string) {
-  const regex = new RegExp(`${q}`, 'gi')
+function replacer(match: string) {
+  return `<mark class="cvt-highlight">${match}</mark>`
+}
+
+function markText(list: Vtt[], q: string) {
   return list.map(item => ({
     ...item,
-    text: `${item.text}`
-      .replace(
-        regex,
-        match => `<mark class="ctv-highlight">${match}</mark>`
-      )
+    text: `${item.text}`.includes(q) ?
+      `${item.text}`.replace(new RegExp(`${q}`, 'gi'), replacer) :
+      `${item.text}`.replace(new RegExp(`${q.match(/\S+/g).join('|')}`, 'gi'), replacer)
   }))
 }
