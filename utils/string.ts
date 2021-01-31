153443d55352d99a2805f6c287f1b5274ca8e457
Jefry Dewangga <jefrydco@gmail.com>
2021-01-31 12:23:43 +0700

    chore: bug fix

--- utils/string.ts
+++ utils/string.ts
@@ -2,7 +2,6 @@ import { stripHtml as strip } from 'string-strip-html'
 
 export function stripHtml(string: string) {
   let { result } = strip(string)
-  result = result.replace(/(<([^>]+)>)/ig, '')
   return result
 }
 
