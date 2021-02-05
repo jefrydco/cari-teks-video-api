98c6bd4d69be2b4109fb75e3df8d594bc455de18
Jefry Dewangga <jefrydco@gmail.com>
2021-02-05 23:01:15 +0700

    chore: log meta

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -4,13 +4,25 @@ require('es6-promise').polyfill()
 require('isomorphic-fetch')
 
 export async function getVTT(url: string) {
-  const res = await fetch(url).then((_) => _.text())
+  const res = await fetch(url).then((_) => (_.ok ? _.text() : ''))
   return res
 }
 
 export async function getJson(url: string): Promise<VttWithMeta> {
   const res = await fetch(url)
-    .then((_) => _.json())
+    .then((_) =>
+      _.ok
+        ? _.json()
+        : {
+            data: [],
+            meta: {
+              title: '',
+              channelName: '',
+              channelUrl: '',
+              channelLogoUrl: ''
+            }
+          }
+    )
     .then(({ data, meta }) => ({ data, meta }))
   return res
 }
